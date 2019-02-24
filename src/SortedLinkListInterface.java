4f2ff54fc58b4748fb1fcdacb6037d5427fd79e2
Ye <yewang8864@gmail.com>
2019-02-24 00:05:05 -0500

    git went bad .... need to redo chanages ....

--- src/SortedLinkListInterface.java
+++ src/SortedLinkListInterface.java
@@ -1,8 +1,8 @@
 
 public interface SortedLinkListInterface {
 
-	void insert(SortedLinkedList list, Node node);
-	void delete(SortedLinkedList list, Node node);
+	void insert(SortedLinkedList list, int data);
+	void delete(SortedLinkedList list, int data);
 	void clearAll(SortedLinkedList list);
 	SortedLinkedList reverseLinkList(SortedLinkedList list);
 	void printLinkedList(SortedLinkedList list);
--- src/SortedLinkedList.java
+++ src/SortedLinkedList.java
@@ -1,20 +1,52 @@
 
-public class SortedLinkedList {
+public class SortedLinkedList implements SortedLinkListInterface{
 	public Node head; 
 	public int size; 
 	public SortedLinkedList(){
 		this.head = null; 
 		this.size=0;
 	}
-	
-//	sorted Linkedlist 
-	public void Insert(SortedLinkedList list,int data){
-		
-		if(list.head == null ){
-			this.head = new Node(data);
-		}else if( ) {
+
+	@Override
+	public void insert(SortedLinkedList list, int data) {
+		this.size++ ;
+		Node newNode = new Node(data);
+		if(list.head==null){
+			list.head = newNode;
+		}else if(this.head.data>newNode.data){
+			newNode.next = list.head;
+			list.head = newNode;
+		}else{
+			Node cur = list.head ; 
+			Node pre = null;
+			
 			
 		}
+		
+		
+	}
+
+	@Override
+	public void delete(SortedLinkedList list, int data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearAll(SortedLinkedList list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SortedLinkedList reverseLinkList(SortedLinkedList list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printLinkedList(SortedLinkedList list) {
+		// TODO Auto-generated method stub
 		
 	}
 
